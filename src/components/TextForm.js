d50fb6a887d944465ba7ebd30ef5e1c45d74d1bb
Osama13088 <firebase946@gmail.com>
2024-09-30 23:32:50 +0500

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,47 +1,78 @@
-import React, {useState} from 'react'
+import React, {useState} from "react";
 
 export default function TextForm(props) {
-
-    const handleUpClick = () =>{
+    const handleUpClick = () => {
         let newText = text.toUpperCase();
-        setText(newText)
-    }
+        setText(newText);
+    };
 
-    const handleLowClick = () =>{
+    const handleLowClick = () => {
         let newText = text.toLowerCase();
-        setText(newText)
-    }
+        setText(newText);
+    };
 
-    const handleClearClick = () =>{
-        let newText = ''; 
-        setText(newText)
-    }
+    const handleClearClick = () => {
+        let newText = "";
+        setText(newText);
+    };
 
     const handleOnChange = (event) => {
-        setText(event.target.value)
-    }
+        setText(event.target.value);
+    };
+
+    const handleCopy = () => {
+        let text = document.getElementById("description");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    };
+
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+    };
 
     const [text, setText] = useState("");
-  return (
-    <>
-    <div className="container">
-    <h3 className="text-center mt-2">{props.heading}</h3>
-  <div className="mb-3">
-    <label htmlFor="description" className="form-label">Description</label>
-    <textarea className="form-control" value={text} rows="8" onChange={handleOnChange} id="description"></textarea>
-  </div>
-  <button type="submit" onClick={handleUpClick} className="btn btn-primary mx-1">Convert to Upper Case</button>
-  <button type="submit" onClick={handleLowClick} className="btn btn-primary mx-1">Convert to Lower Case</button>
-  <button type="submit" onClick={handleClearClick} className="btn btn-primary mx-1">Clear Text</button>
-
-    </div>
-    <div className="container my-3">
-        <h2>Your Text summary</h2>
-        <p>Total Words {text.split(" ").length} and Total Characters {text.length}</p>
-        <p>Total Characters without Space: {text.length - text.split(" ").length}</p>
-        <h3>Preview</h3>
-        <p>{text}</p>
-    </div>
-    </>
-  )
+    return (
+        <>
+            <div className="container">
+                <h3 className="text-center mt-2">{props.heading}</h3>
+                <div className="mb-3">
+                    <label htmlFor="description" className="form-label">
+                        Description
+                    </label>
+                    <textarea
+                        className="form-control"
+                        value={text}
+                        rows="8"
+                        onChange={handleOnChange}
+                        id="description"
+                    ></textarea>
+                </div>
+                <button type="submit" onClick={handleUpClick} className="btn btn-primary mx-1">
+                    Convert to Upper Case
+                </button>
+                <button type="submit" onClick={handleLowClick} className="btn btn-primary mx-1">
+                    Convert to Lower Case
+                </button>
+                <button type="submit" onClick={handleClearClick} className="btn btn-primary mx-1">
+                    Clear Text
+                </button>
+                <button type="submit" onClick={handleCopy} className="btn btn-primary mx-1">
+                    Copy Text
+                </button>
+                <button type="submit" onClick={handleExtraSpaces} className="btn btn-primary mx-1">
+                    Remove Extra Space
+                </button>
+            </div>
+            <div className="container my-3">
+                <h2>Your Text summary</h2>
+                <p>
+                    Total Words {text.split(" ").length} and Total Characters {text.length}
+                </p>
+                <p>Total Characters without Space: {text.length - text.split(" ").length}</p>
+                <h3>Preview</h3>
+                <p>{text}</p>
+            </div>
+        </>
+    );
 }

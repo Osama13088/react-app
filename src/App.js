076a16e984fdcea32795afa8e015bf811de6a201
Osama13088 <firebase946@gmail.com>
2024-09-30 22:59:38 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import About from "./components/About";
 import Navbar from "./components/Navbar";
-// import TextForm from "./components/TextForm";
+import TextForm from "./components/TextForm";
 
 function App() {
-  return (
-    <>
-    <Navbar aboutText="About React"/>
-    <div className="container">
-    {/* <TextForm heading="Enter your Data"/> */}
-    <About/>
-    </div>
-    
-    </>
-  );
+    return (
+        <>
+            <Navbar aboutText="About React" />
+            <div className="container">
+                <TextForm heading="Enter your Data" />
+                {/* <About/> */}
+            </div>
+        </>
+    );
 }
 
 export default App;

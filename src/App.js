124a19cb2180c2b7c1b715ea922aa8c9f426e2c0
Osama13088 <firebase946@gmail.com>
2024-10-01 00:11:51 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 // import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import React, {useState} from "react";
 
 function App() {
+    const [mode, setMode] = useState("light");
+    const toggleMode = () => {
+        if (mode === "light") {
+            setMode("dark");
+        } else {
+            setMode("light");
+        }
+    };
     return (
         <>
-            <Navbar aboutText="About React" />
+            <Navbar aboutText="About React" mode={mode} toggleMode={toggleMode} />
             <div className="container">
                 <TextForm heading="Enter your Data" />
                 {/* <About/> */}
